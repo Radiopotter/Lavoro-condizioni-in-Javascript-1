8719783c1142d3d2937698fbf50b0f59d73d004d
Sophien Mejri <mejrisophien464@gmail.it>
2026-06-30 17:00:25 +0200

    Esercizio Javascript

--- Lavorocondizioni.js
+++ Lavorocondizioni.js
@@ -1,17 +1,56 @@
 let v = 29;
 
+// IF ELSE
 if (v < 18) {
     console.log("Insufficiente");
-} else if (v >= 18 && v < 21) {
+} else if (v < 21) {
     console.log("Sufficiente");
-} else if (v >= 21 && v < 24) {
+} else if (v < 24) {
     console.log("Buono");
-} else if (v >= 24 && v < 27) {
+} else if (v < 27) {
     console.log("Distinto");
-} else if (v >= 27 && v <= 29) {
+} else if (v <= 29) {
     console.log("Ottimo");
 } else if (v === 30) {
     console.log("Eccellente");
 } else {
     console.log("Voto non valido");
+}
+
+// SWITCH
+switch (v) {
+    case 30:
+        console.log("Eccellente");
+        break;
+
+    case 27:
+    case 28:
+    case 29:
+        console.log("Ottimo");
+        break;
+
+    case 24:
+    case 25:
+    case 26:
+        console.log("Distinto");
+        break;
+
+    case 21:
+    case 22:
+    case 23:
+        console.log("Buono");
+        break;
+
+    case 18:
+    case 19:
+    case 20:
+        console.log("Sufficiente");
+        break;
+
+    default:
+        if (v < 18) {
+            console.log("Insufficiente");
+        } else {
+            console.log("Voto non valido");
+        }
 }
